a22b4f5e874793a8196bd4a23b728d10b1d9a523
Georgey764 <georgesamuel764@gmail.com>
2025-11-20 15:27:29 -0600

    backend updates

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,76 @@
 from processing_classes import ResumeProcessor
+from sentence_transformers import SentenceTransformer
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.middleware.cors import CORSMiddleware
+import shutil
+import os
+from typing import Annotated
+import csv
+from jobspy import scrape_jobs
+
+app = FastAPI()
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+os.makedirs("uploads", exist_ok=True)
+
+def give_scraped_jobs(job_title):
+    jobs = scrape_jobs(
+        site_name=["indeed", "linkedin", "zip_recruiter", "google"], # "glassdoor", "bayt", "naukri", "bdjobs"
+        search_term=job_title,
+        google_search_term="software engineer jobs near San Francisco, CA since yesterday",
+        location="San Francisco, CA",
+        results_wanted=20,
+        hours_old=72,
+        country_indeed='USA',
+        
+        # linkedin_fetch_description=True # gets more info such as description, direct job url (slower)
+        # proxies=["208.195.175.46:65095", "208.195.175.45:65095", "localhost"],
+    )
+    print(f"Found {len(jobs)} jobs")
+    print(jobs.head())
+    jobs.to_csv("jobs.csv", quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False)
+
+@app.post("/submit-job-application/")
+def submit_application(
+    resume: Annotated[UploadFile, File()], 
+    job_title: Annotated[str, Form()]     
+):
+    save_path = f"uploads/{resume.filename}"
+
+    with open(save_path, "wb") as buffer:
+        shutil.copyfileobj(resume.file, buffer)
+        
+    resume_processor = ResumeProcessor(save_path, model)
+    
+    
+    
+    print(similarity_score)
+
+    return {
+        "status": "success", 
+        "filename": resume.filename, 
+        "similarity_score": similarity_score
+    }
+
+
 
 def main():
     file_path = "/Users/george/Documents/Resume/2025/ULM CRM Database Admin/Copy-Resume.docx"
     processor = ResumeProcessor(file_path)
 
     print(processor.get_similarity_score("""We are looking for a Data Scientist with experience in Python, Machine Learning, 
-and Deep Learning. Must be familiar with PyTorch and Scikit-Learn. 
-Experience with NLP is a plus."""))
+    and Deep Learning. Must be familiar with PyTorch and Scikit-Learn. 
+    Experience with NLP is a plus."""))
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+    
--- backend/processing_classes.py
+++ backend/processing_classes.py
@@ -1,9 +1,27 @@
-from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import PyPDF2
 import docx2txt
 import re
+import csv
+from jobspy import scrape_jobs
+
+def give_scraped_jobs(job_title):
+    jobs = scrape_jobs(
+        site_name=["indeed", "linkedin", "zip_recruiter", "google"], # "glassdoor", "bayt", "naukri", "bdjobs"
+        search_term=job_title,
+        google_search_term="software engineer jobs near San Francisco, CA since yesterday",
+        location="San Francisco, CA",
+        results_wanted=20,
+        hours_old=72,
+        country_indeed='USA',
+        
+        # linkedin_fetch_description=True # gets more info such as description, direct job url (slower)
+        # proxies=["208.195.175.46:65095", "208.195.175.45:65095", "localhost"],
+    )
+    print(f"Found {len(jobs)} jobs")
+    print(jobs.head())
+    jobs.to_csv("jobs.csv", quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False)
 
 class ResumeProcessor:
     def _extract_text_from_file(self, file_path):
@@ -36,9 +54,8 @@ class ResumeProcessor:
         
         return text
     
-    def __init__(self, file_name):
-        print("Initializing AI model...")
-        self.model = SentenceTransformer('all-MiniLM-L6-v2')
+    def __init__(self, file_name, model):
+        self.model = model
         text = self._extract_text_from_file(file_name)
         self.resume_text = self._clean_text(text)
         
